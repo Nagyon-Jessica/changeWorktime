45257c2bb4941e2e51f6d8b90a090160da178887
yagaodekawasu <40302128+Nagyon-Jessica@users.noreply.github.com>
2022-01-09 19:14:10 +0900

    Import TimeoutException

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
